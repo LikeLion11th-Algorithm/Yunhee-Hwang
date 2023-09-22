815e2d21208ed013063ffb397b7edb2d990542c6
yunhee1 <alkrg007@naver.com>
2023-09-23 00:22:27 +0900

    solved: [PGS] 181942 문자열 섞기

---
+++ programmers/181942: 문자열 섞기.py
@@ -0,0 +1,25 @@
+# 길이가 같은 두 문자열 str1과 str2가 주어집니다.
+# 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
+
+# sol
+def solution(str1, str2):
+    answer = []
+
+    for i in range(len(str1)):
+        answer.append(str1[i])
+        answer.append(str2[i])
+    return ''.join(answer)
+
+
+# sol2
+def solution(str1, str2):
+    answer = ''
+    for i in range(0, len(str1)):
+        answer = answer + str1[i] + str2[i]
+    return answer
+
+
+# sol3
+def solution(str1, str2):
+    answer = ''.join([str1[i]+str2[i] for i in range(len(str1))])
+    return answer
