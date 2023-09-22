3b28797060b8833d8c1f67263829466c073032c7
yunhee1 <alkrg007@naver.com>
2023-09-23 00:29:22 +0900

    solved: [PGS] 181941 문자 리스트를 문자열로 변환하기

---
+++ programmers/181941: 문자 리스트를 문자열로 변환하기.py
@@ -0,0 +1,9 @@
+# 문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+
+# sol
+def solution(arr):
+    return ''.join(arr)
+
+
+# sol2
+# solution = lambda l : ''.join(l) # 그냥 쓰면 자꾸 수정돼서 주석 안에 씀.
