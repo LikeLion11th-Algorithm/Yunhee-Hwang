4549f9efeab624b027877b5b1fc9f52579a584f5
yunhee1 <alkrg007@naver.com>
2023-09-22 20:53:37 +0900

    solved: [PGS] 181899 카운트 다운

---
+++ programmers/181899: 카운트 다운.py
@@ -0,0 +1,17 @@
+# 정수 start_num와 end_num가 주어질 때, start_num에서 end_num까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+# sol
+def solution(start, end_num):
+    answer = []
+
+    for i in range(end_num, start+1):
+        answer.append(i)
+
+    answer.sort(reverse=True)
+    return answer
+
+# sol2
+
+
+def solution(start, end_num):
+    return list(range(start, end_num-1, -1))
