8e37ca4008e4073182749fe3ed1611954fc94dcf
yunhee1 <alkrg007@naver.com>
2023-11-30 23:51:47 +0900

    solved :[CPEQ] 112901: raindrop

---
+++ corporate-past-exam-questions/112901: raindrop.py
@@ -0,0 +1,25 @@
+def solution(bricks):
+
+	answer = 0
+	n = len(bricks)
+
+	left_max_heights = [0] * n
+	right_max_heights = [0] * n
+
+	left_max = 0
+	for i in range(n):
+	    left_max = max(left_max, bricks[i])
+	    left_max_heights[i] = left_max
+
+	right_max = 0
+	for i in range(n - 1, -1, -1):
+	    right_max = max(right_max, bricks[i])
+	    right_max_heights[i] = right_max
+
+	for i in range(1, n - 1):
+	    min_height = min(left_max_heights[i], right_max_heights[i])
+	    if min_height > bricks[i]:
+	        answer += min_height - bricks[i]
+
+
+    return answer
