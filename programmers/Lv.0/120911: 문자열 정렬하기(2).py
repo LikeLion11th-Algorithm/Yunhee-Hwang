fed05ee0394f479fa9ad21978fc0cb5b378cd011
yunhee1 <alkrg007@naver.com>
2023-12-27 22:05:29 +0900

    solved: [PGS] 120911: 문자열 정렬하기(2)

---
+++ programmers/Lv.0/120911: 문자열 정렬하기(2).py
@@ -0,0 +1,10 @@
+# sol
+
+def solution(my_string):
+    my_string = my_string.lower()
+    return ''.join(sorted(my_string))
+
+
+# sol2
+def solution(my_string):
+    return ''.join(sorted(my_string.lower()))
