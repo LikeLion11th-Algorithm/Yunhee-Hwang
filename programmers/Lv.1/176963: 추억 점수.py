c082eb058224ecbcd82dc23dc32646ac758379d4
yunhee1 <alkrg007@naver.com>
2024-05-07 23:34:01 +0900

    solved-update: [PGS] 176963: 추억점수

--- programmers/Lv.1/176963: 추억 점수.py
+++ programmers/Lv.1/176963: 추억 점수.py
@@ -12,5 +12,19 @@ def solution(name, yearning, photo):
             if pho[j] in dict:
                 s += dict[pho[j]]
         answer.append(s)
-    # print(dict)
+    return answer
+
+
+# sol2
+def solution(name, yearning, photo):
+    answer = []
+    # zip, for문으로 줄인 버전
+    dic = {x: y for x, y in list(zip(name, yearning))}
+
+    for pho in photo:
+        s = 0
+        for j in range(len(pho)):
+            if pho[j] in dic:
+                s += dic[pho[j]]
+        answer.append(s)
     return answer
